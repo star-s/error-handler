b44748c7c98bf8bedbe3a3c34f6456ba7ea2b2b1
Sergey Starukhin <star.s@me.com>
2024-03-04 09:06:30 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ErrorHandler",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ErrorHandler",
+            targets: ["ErrorHandler"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ErrorHandler"),
+        .testTarget(
+            name: "ErrorHandlerTests",
+            dependencies: ["ErrorHandler"]),
+    ]
+)
